938456b23b9bd38163411f4704c6a149272e8ccd
bigginlasley <bigginlasley@gmail.com>
2018-10-31 14:37:34 -0600

    added string to mainactivity
    added a string to mainactivity

--- app/src/main/java/com/example/evlrhawk/digitaljukebox/MainActivity.java
+++ app/src/main/java/com/example/evlrhawk/digitaljukebox/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+<<<<<<< Updated upstream
 
 //    what up dog?
     // Que bueno!
     // Not much, what's up with you?
 
+=======
+>>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String string = "String";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
