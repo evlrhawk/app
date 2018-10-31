f2c904505fda22dd2c219a79ff26b694476341f4
bigginlasley <bigginlasley@gmail.com>
2018-10-31 14:39:42 -0600

    Revert "added string to mainactivity"
    This reverts commit 938456b23b9bd38163411f4704c6a149272e8ccd.

--- app/src/main/java/com/example/evlrhawk/digitaljukebox/MainActivity.java
+++ app/src/main/java/com/example/evlrhawk/digitaljukebox/MainActivity.java
@@ -4,17 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-<<<<<<< Updated upstream
 
 //    what up dog?
     // Que bueno!
     // Not much, what's up with you?
 
-=======
->>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String string = "String";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
