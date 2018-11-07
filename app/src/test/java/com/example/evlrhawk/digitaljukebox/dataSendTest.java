638268ee4f7ffe5fdfca01448f78fbd8220f5dc2
elkmeat12 <38544362+elkmeat12@users.noreply.github.com>
2018-11-07 15:01:39 -0700

    Added Test to dataSendTest
    Added placeholder code to the dataSendTest.java

--- app/src/test/java/com/example/evlrhawk/digitaljukebox/dataSendTest.java
+++ app/src/test/java/com/example/evlrhawk/digitaljukebox/dataSendTest.java
@@ -1,4 +1,12 @@
 package com.example.evlrhawk.digitaljukebox;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class dataSendTest {
+    MainActivity data;
+
+    @Test
+    public void dateSent() {assertEquals(true, data);}
 }
