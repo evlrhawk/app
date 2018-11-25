46cf41a4f3d000652cefc5ae8c87b54dedaf17fc
bigginlasley <bigginlasley@gmail.com>
2018-11-25 16:12:56 -0700

    adding login

--- app/src/main/java/com/example/evlrhawk/digitaljukebox/MainActivity.java
+++ app/src/main/java/com/example/evlrhawk/digitaljukebox/MainActivity.java
@@ -24,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+
+
+
         databaseReference = FirebaseDatabase.getInstance().getReference("string");
 
 
