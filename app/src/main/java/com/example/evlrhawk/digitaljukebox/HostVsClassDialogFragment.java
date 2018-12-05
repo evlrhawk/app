045ff5c312340805749cdbe98e43841d96675d2a
Christopher <wil15090@byui.edu>
2018-12-05 14:12:11 -0700

    added the HvG dialog class NOT COMPLETE YET

--- app/src/main/java/com/example/evlrhawk/digitaljukebox/HostVsClassDialogFragment.java
+++ app/src/main/java/com/example/evlrhawk/digitaljukebox/HostVsClassDialogFragment.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
 public class HostVsClassDialogFragment extends DialogFragment {
-    @Override
 
     final String dialog_Host_Vs_Guest = "Do you want to be a host a guest?";
     final String Host = "Host";
     final String Guest = "Guest";
 
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
